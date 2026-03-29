6d336ed6ebcd20efb80bc1b3e22cfc09b51a04f1
samuelsondhrm <samuelsondharmawan31@gmail.com>
2026-03-29 23:46:07 +0700

    feat(token): add toString and tokenTypeName to fulfill output results

--- lexer/token/token.cpp
+++
@@ -1,8 +0,0 @@
-#include "token.hpp" 
-#include <iostream>  
-using namespace std;
-
-
-Token::Token(TokenType type, string value, int line) : type(type), value(value), line(line) { 
-	cout << "Token constructed" << endl; 
-} 
