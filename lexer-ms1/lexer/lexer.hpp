9b5d9073c3830637356347724dc13fd3e01ad266
NikSamSim <nikosimanjuntak71@gmail.com>
2026-03-29 16:13:15 +0700

    feat: lexer

---
+++ lexer-ms1/lexer/lexer.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#include <string>
+#include <vector>
+#include "../token/token.hpp"
+//#include "../dfa/dfa.hpp"
+
+using namespace std;
+
+/**
+ * Lexer
+ * 
+ * Langkah:
+ * - Ambil string
+ * - Ekspos nextToken()
+ * - Ekspos tokenize()
+ */
+
+class Lexer {
+public:
+    explicit Lexer(const string& src);
+
+    // Return next token, serahkan ke DFA
+    Token nextToken();
+
+    // Return all tokens, termasuk EOF.
+    vector<Token> tokenize();
+
+    bool isEOF() const;
+
+private:
+    DFA dfa;
+};
